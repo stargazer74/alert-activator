3b9a2972e6fb855531f9b60f37b9d213760511ec
Stargazer <chris@antisphex.net>
2019-08-20 07:34:41 +0200

    initial spring boot commit

---
+++ src/main/java/de/heimwaechter/alertactivator/AlertactivatorApplication.java
@@ -0,0 +1,13 @@
+package de.heimwaechter.alertactivator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertactivatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertactivatorApplication.class, args);
+	}
+
+}
